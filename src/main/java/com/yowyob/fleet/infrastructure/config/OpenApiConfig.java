cc6b582c2948a6e761b743af309b937b879258e6
KamgaBrayan <brayanarmel@gmail.com>
2026-04-20 14:22:09 +0100

    Dependency import corrections

--- src/main/java/com/yowyob/fleet/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/yowyob/fleet/infrastructure/config/OpenApiConfig.java
@@ -4,10 +4,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.beans.factory.annotation.Value; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
